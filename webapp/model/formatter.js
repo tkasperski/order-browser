4c070a214830d5a186e4be2a0d17e1b68945fbf5
Tomasz Kasperski <tkasperski98@gmail.com>
2021-05-11 06:53:40 +0000

    Master View updated

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -15,6 +15,54 @@ sap.ui.define([], function () {
 			}
 
 			return parseFloat(sValue).toFixed(2);
+        },
+        
+        /**
+		 * Provides a semantic state to indicate the delivery status based on shipped and required dates
+		 *
+		 * @public
+		 * @param {object} oRequiredDate required date of the order
+		 * @param {object} oShippedDate shipped date of the order
+		 * @returns {string} semantic state of the order
+		 */
+		deliveryState: function (oRequiredDate, oShippedDate) {
+			if (oShippedDate === null) {
+				return "None";
+			}
+
+			// delivery is urgent (takes more than 7 days)
+			if (oRequiredDate - oShippedDate > 0 && oRequiredDate - oShippedDate <= 432000000) {
+				return "Warning";
+			} else if (oRequiredDate < oShippedDate) { // delivery is too late
+				return "Error";
+			} else { // delivery is in time
+				return "Success";
+			}
+        },
+        
+        /**
+		 * Provides a text to indicate the delivery status based on shipped and required dates
+		 *
+		 * @public
+		 * @param {object} oRequiredDate required date of the order
+		 * @param {object} oShippedDate shipped date of the order
+		 * @returns {string} delivery status text from the resource bundle
+		 */
+		deliveryText: function (oRequiredDate, oShippedDate) {
+			var oResourceBundle = this.getModel("i18n").getResourceBundle();
+
+			if (oShippedDate === null) {
+				return "None";
+			}
+
+			// delivery is urgent (takes more than 7 days)
+			if (oRequiredDate - oShippedDate > 0 && oRequiredDate - oShippedDate <= 432000000) {
+				return oResourceBundle.getText("formatterDeliveryUrgent");
+			} else if (oRequiredDate < oShippedDate) { //d elivery is too late
+				return oResourceBundle.getText("formatterDeliveryTooLate");
+			} else { // delivery is in time
+				return oResourceBundle.getText("formatterDeliveryInTime");
+			}
 		}
 	};
 });
